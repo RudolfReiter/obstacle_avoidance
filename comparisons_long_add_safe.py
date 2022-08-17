18f9ce73161ce921d1e05e7493f850420987eabc
Rudolf <rudolf.reiter@student.tugraz.at>
2022-08-17 18:41:25 +0200

    plots as they should look like

--- comparisons_long_add_safe.py
+++ comparisons_long_add_safe.py
@@ -22,10 +22,12 @@ if __name__ == "__main__":
     print("-----------------------")
 
     # general scenario parameters
+    SAVE = True
+    f_name = "save_data_long_add_safe_2"
     USE_RANDOM = False
     ANIMATE = False
-    n_sim = int(45)
-    n_runs = 20
+    n_sim = int(80)
+    n_runs = 10
     idx_skip_traj = 2
     validations = []
     planner_options = [
@@ -57,8 +59,8 @@ if __name__ == "__main__":
         ego_model_params.maximum_acceleration_force = 15e3
         ego_model_params.maximum_velocity = 40
         ego_model_params.maximum_lateral_acc = 18
-        ego_model_params.length_rear = 2
-        ego_model_params.length_front = 2
+        ego_model_params.length_rear = 1.7
+        ego_model_params.length_front = 1.7
         if not options.lifting and options.obstacle == VehicleObstacleModel.ELIPSE:
             ego_model_params.safety_radius = 3.5
         else:
@@ -73,11 +75,10 @@ if __name__ == "__main__":
         opp_model_params_0.maximum_lateral_acc = 12
         opp_model_params_0.chassis_length = 26
         opp_model_params_0.chassis_width = 2.8
-        opp_model_params_0.length_rear = 3
-        opp_model_params_0.length_front = 3
+        opp_model_params_0.length_rear = 10
+        opp_model_params_0.length_front = 10
 
         opp_model_params_1 = deepcopy(opp_model_params_0)
-        opp_model_params_2 = deepcopy(opp_model_params_0)
 
         # Create planner options
         planner_options = PlannerOptions()
@@ -91,7 +92,7 @@ if __name__ == "__main__":
         planner_options.n_circles_ego = options.circles
         if options.lifting:
             planner_options.auto_size_circles = True
-            planner_options.increase_opp = 0.5
+            planner_options.increase_opp = 0.0
         else:
             planner_options.auto_size_circles = False
             planner_options.increase_opp = 2.0
@@ -106,9 +107,9 @@ if __name__ == "__main__":
         # Create Animation Parameter
         animation_parameter = AnimationParameters()
         animation_parameter.animation_frames_per_sec = 10
-        animation_parameter.animation_speed = 1.
+        animation_parameter.animation_speed = 3.
         animation_parameter.plot_opp_predictions = [0]
-        animation_parameter.plot_safety_circles = [0, 1, 2, 3]
+        animation_parameter.plot_safety_circles = [0, 1, 2]
         animation_parameter.plot_ego_plans = [0]
         animation_parameter.plot_acceleration_arrows = [0]
         animation_parameter.planning_color_type = AnimationPlanningColorType.UNI
@@ -116,67 +117,53 @@ if __name__ == "__main__":
 
         # Create test road
         road_options = RoadOptions()
-        road_options.road_width = 12
+        road_options.road_width = 15
         road_options.n_points = 400
-        road_options.random_road_parameters.maximum_kappa = 1 / 30
+        road_options.random_road_parameters.maximum_kappa = 1 / 900
         road = Road(road_options)
         road.randomize(seed=-1)
-        road.set_kappa(kappa_grid=road.kappa_grid_)  # +(-1/50 + np.cumsum(np.ones_like(road.kappa_grid_))/15000))
+        road.set_kappa(kappa_grid=road.kappa_grid_ + (-1 / 50 + np.cumsum(np.ones_like(road.kappa_grid_)) / 15000))
 
         # Create planner
         vehicle_planner_ego = VehiclePlannerAcados20222(ego_model_params=ego_model_params,
                                                         road=road,
                                                         planner_options=planner_options,
-                                                        opp_model_params=[opp_model_params_0, opp_model_params_1,
-                                                                          opp_model_params_2])
+                                                        opp_model_params=[opp_model_params_0, opp_model_params_1])
 
         vehicle_planner_opp_0 = VehiclePlannerAcados20222(ego_model_params=opp_model_params_0,
                                                           road=road,
                                                           planner_options=opp_planner_options,
-                                                          opp_model_params=[ego_model_params, opp_model_params_1,
-                                                                            opp_model_params_2])
+                                                          opp_model_params=[ego_model_params, opp_model_params_1])
 
         vehicle_planner_opp_1 = VehiclePlannerAcados20222(ego_model_params=opp_model_params_1,
                                                           road=road,
                                                           planner_options=opp_planner_options,
-                                                          opp_model_params=[ego_model_params, opp_model_params_0,
-                                                                            opp_model_params_2])
-
-        vehicle_planner_opp_2 = VehiclePlannerAcados20222(ego_model_params=opp_model_params_1,
-                                                          road=road,
-                                                          planner_options=opp_planner_options,
-                                                          opp_model_params=[ego_model_params, opp_model_params_0,
-                                                                            opp_model_params_1])
+                                                          opp_model_params=[ego_model_params, opp_model_params_0])
 
         speed_opp = 15
-        dist = 45.
-        ini_dist = 50
-        n_pos = 3
+        dist = 60.
+        ini_dist = 100
+        n_pos = -0
         # Set parameters
         initial_state_ego_f = np.array([1, 0., 0., speed_opp - 5, 0])
-        initial_state_opp_0_f = np.array([ini_dist, -n_pos, 0.0, speed_opp, 0])
+        initial_state_opp_0_f = np.array([ini_dist, n_pos, 0.0, speed_opp, 0])
         initial_state_opp_1_f = np.array([ini_dist + dist, n_pos, 0.0, speed_opp, 0])
-        initial_state_opp_2_f = np.array([ini_dist + 2 * dist + 20, -n_pos, 0.0, speed_opp, 0])
 
         # Default actions
         action0 = np.array([0., 50., 1e2])
-        action1 = np.array([-n_pos, speed_opp, 1e5])
+        action1 = np.array([n_pos, speed_opp, 1e5])
         action2 = np.array([n_pos, speed_opp, 1e5])
-        action3 = np.array([-n_pos, speed_opp, 1e5])
 
         # Set parameters
         simulator_options = SimulatorOptions()
         simulator_options.n_sim = int(n_sim)
         simulator_options.idx_skip_traj = idx_skip_traj
         simulator = SimpleSimulator(options=simulator_options,
-                                    initial_states=[initial_state_ego_f, initial_state_opp_0_f, initial_state_opp_1_f,
-                                                    initial_state_opp_2_f],
-                                    vehicle_parameters=[ego_model_params, opp_model_params_0, opp_model_params_1,
-                                                        opp_model_params_2],
-                                    planners=[vehicle_planner_ego, vehicle_planner_opp_0, vehicle_planner_opp_1,
-                                              vehicle_planner_opp_2],
+                                    initial_states=[initial_state_ego_f, initial_state_opp_0_f, initial_state_opp_1_f],
+                                    vehicle_parameters=[ego_model_params, opp_model_params_0, opp_model_params_1],
+                                    planners=[vehicle_planner_ego, vehicle_planner_opp_0, vehicle_planner_opp_1],
                                     road=road,
-                                    default_actions=[action0, action1, action2, action3])
+                                    default_actions=[action0, action1, action2])
 
         t_full, t_average, t_max, t_min, collisions_lead, collisions_follow, min_distances = \
             [], [], [], [], [], [], []
@@ -188,10 +175,8 @@ if __name__ == "__main__":
             if ANIMATE:
                 animator = Animator(animation_parameter=animation_parameter)
                 animator.set_data(planning_data_container=simulator.planning_containers,
-                                  vehicle_parameter=[ego_model_params, opp_model_params_0, opp_model_params_1,
-                                                     opp_model_params_2],
-                                  planner_parameter=[planner_options, opp_planner_options, opp_planner_options,
-                                                     opp_planner_options],
+                                  vehicle_parameter=[ego_model_params, opp_model_params_0, opp_model_params_1],
+                                  planner_parameter=[planner_options, opp_planner_options, opp_planner_options],
                                   road=road,
                                   statistics=simulator.statistics)
                 animator.animate(save_as_movie=False)
@@ -211,17 +196,17 @@ if __name__ == "__main__":
             qp_iter.append(np.mean(simulator.statistics.qp_iters))
             s_final.append(simulator.current_states[0][0])
 
-            road.randomize(seed=i)
+            #road.randomize(seed=i)
             np.random.seed(seed=i)
-            delta_ss = np.random.uniform(-5, 5, 3)
+            delta_ss = np.random.uniform(-15, 35, 3)
+            if delta_ss[0]+60<delta_ss[1]:
+                delta_ss[1]=delta_ss[0]+60
 
             initial_state_opp_0_f[0] = initial_state_opp_0_f[0] + delta_ss[0]
             initial_state_opp_1_f[0] = initial_state_opp_1_f[0] + delta_ss[1]
-            initial_state_opp_2_f[0] = initial_state_opp_2_f[0] + delta_ss[2]
-            simulator.reset([initial_state_ego_f, initial_state_opp_0_f, initial_state_opp_1_f,
-                             initial_state_opp_2_f], road=road)
+            simulator.reset([initial_state_ego_f, initial_state_opp_0_f, initial_state_opp_1_f], road=road)
 
-        collisions = np.sum(np.array(collisions_lead + collisions_follow) > 0)
+        collisions = float(np.sum(np.array(collisions_lead + collisions_follow) > 0))
         label_class = "20222"
         validations.append(ValidationResult(obstacle_avoidance_label=options.str_obstacle,
                                             class_label=label_class,
@@ -257,6 +242,6 @@ if __name__ == "__main__":
         table_output.append(table_row)
     t = tabulate(table_output, headers=table_header, tablefmt='orgtbl')
     print(t)
-
-    with open("save_data_long_add_safe", "wb") as fp:  # Pickling
-        pickle.dump(validations, fp)
+    if SAVE:
+        with open(f_name, "wb") as fp:  # Pickling
+            pickle.dump(validations, fp)
--- plotter.py
+++ plotter.py
@@ -25,7 +25,8 @@ def latexify(fontsize: int = 10):
     matplotlib.rcParams.update(params)
 
 
-def boxplot_timings(computation_times: List[np.ndarray], labels: List[str], filename: str):
+def boxplot_timings(data_set: List[np.ndarray], labels: List[str], filename: str, y_axis_label: str,
+                    crashes: List[bool] = None):
     latexify()
     fig, ax = plt.subplots(figsize=(4, 4))
     ax = plt.gca()
@@ -33,24 +34,24 @@ def boxplot_timings(computation_times: List[np.ndarray], labels: List[str], file
     ax.grid(True, linestyle='dotted')
     ax.set_axisbelow(True)
 
-    plt.ylabel('Computation Time (s)')
+    plt.ylabel(y_axis_label)
 
-    boxplots = plt.boxplot(computation_times,
-                           sym="r+",
-                           labels=[''] * len(computation_times),
-                           notch=False,
-                           # vert=True,
-                           whis=1e3,
-                           showmeans=True,
-                           #           bootstrap=None,
-                           #           usermedians=None,
-                           #           conf_intervals=None,
-                           patch_artist=True,
-                           meanline=True,
-                           meanprops={"linewidth": 0.5, "linestyle": "solid", "color": "black"},
-                           medianprops={"linewidth": 0.5, "linestyle": (0, (1, 0.5)), "color": "black"},
-                           boxprops={"linewidth": 1, "facecolor": "whitesmoke"}
-                           )
+    boxplots = ax.boxplot(data_set,
+                          sym="r+",
+                          labels=[''] * len(data_set),
+                          notch=False,
+                          whis=1e3,
+                          showmeans=True,
+                          patch_artist=True,
+                          meanline=True,
+                          meanprops={"linewidth": 0.5, "linestyle": "solid", "color": "black"},
+                          medianprops={"linewidth": 0.5, "linestyle": (0, (1, 0.5)), "color": "black"},
+                          boxprops={"linewidth": 1, "facecolor": "whitesmoke"}
+                          )
+    for i, box in enumerate(boxplots['boxes']):
+        if crashes[i] > 0:
+            box.set_facecolor("tab:red")
+
     # ax.add_patch(Rectangle((0, 0), 0, 0, facecolor=facecolors[2],  label=label, alpha=0.3))
     plt.rcParams['hatch.linewidth'] = 0.5
     plt.xticks(np.arange(len(labels)) + 1, labels)
@@ -67,23 +68,50 @@ def boxplot_timings(computation_times: List[np.ndarray], labels: List[str], file
 if __name__ == "__main__":
     labels = ["e-l", "e", "5c-l", "5c", "3c-l", "3c", "hp-l", "hp"]
 
-    with open("save_data_long_add_safe", "rb") as fp:  # Unpickling
+    with open("save_data_long_add_safe_2", "rb") as fp:  # Unpickling
         b = pickle.load(fp)
     timings = [validation.comp_times for validation in b]
     timings = [np.array(timing).flatten() for timing in timings]
-    # labels = [validation.obstacle_avoidance_label + str(validation.lifting_used) for validation in b]
-    boxplot_timings(timings, labels=labels, filename="timings_long_add_safe")
+    crashes = [validation.collisions > 0 for validation in b]
+    boxplot_timings(timings, labels=labels, filename="timings_long_add_safe", y_axis_label='Computation Time (s)',
+                    crashes=crashes)
 
-    with open("save_data_short_add_safe", "rb") as fp:  # Unpickling
+    with open("save_data_long_add_safe_2", "rb") as fp:  # Unpickling
         b = pickle.load(fp)
-    timings = [validation.comp_times for validation in b]
-    timings = [np.array(timing).flatten() for timing in timings]
-    # labels = [validation.obstacle_avoidance_label + str(validation.lifting_used) for validation in b]
-    boxplot_timings(timings, labels=labels, filename="timings_short_add_safe")
+    s_values = [validation.s_final_all for validation in b]
+    s_values = [np.array(s).flatten() for s in s_values]
+    crashes = [validation.collisions > 0 for validation in b]
+    boxplot_timings(s_values, labels=labels, filename="s_values_long_add_safe", y_axis_label='Maximum Progress (m)',
+                    crashes=crashes)
 
-    with open("save_data_long_no_safe", "rb") as fp:  # Unpickling
+    with open("save_data_short_safe_2", "rb") as fp:  # Unpickling
         b = pickle.load(fp)
     timings = [validation.comp_times for validation in b]
     timings = [np.array(timing).flatten() for timing in timings]
-    # labels = [validation.obstacle_avoidance_label + str(validation.lifting_used) for validation in b]
-    boxplot_timings(timings, labels=labels, filename="timings_long_no_safe")
+    crashes = [validation.collisions > 0 for validation in b]
+    boxplot_timings(timings, labels=labels, filename="timings_short_add_safe", y_axis_label='Computation Time (s)',
+                    crashes=crashes)
+
+    with open("save_data_short_safe_2", "rb") as fp:  # Unpickling
+        b = pickle.load(fp)
+    s_values = [validation.s_final_all for validation in b]
+    s_values = [np.array(s).flatten() for s in s_values]
+    crashes = [validation.collisions > 0 for validation in b]
+    boxplot_timings(s_values, labels=labels, filename="s_values_short_add_safe", y_axis_label='Maximum Progress (m)',
+                    crashes=crashes)
+
+    with open("save_data_long_no_safe_2", "rb") as fp:  # Unpickling
+        b = pickle.load(fp)
+    timings = [validation.comp_times for validation in b]
+    timings = [np.array(timing).flatten() for timing in timings]
+    crashes = [validation.collisions > 0 for validation in b]
+    boxplot_timings(timings, labels=labels, filename="timings_long_no_safe", y_axis_label='Computation Time (s)',
+                    crashes=crashes)
+
+    with open("save_data_long_no_safe_2", "rb") as fp:  # Unpickling
+        b = pickle.load(fp)
+    s_values = [validation.s_final_all for validation in b]
+    s_values = [np.array(s).flatten() for s in s_values]
+    crashes = [validation.collisions > 0 for validation in b]
+    boxplot_timings(s_values, labels=labels, filename="s_values_long_no_safe", y_axis_label='Maximum Progress (m)',
+                    crashes=crashes)
